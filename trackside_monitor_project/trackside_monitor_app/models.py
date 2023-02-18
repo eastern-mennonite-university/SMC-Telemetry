bb219a1e8c17ea10a396200d84a3f6efcf344c66
boa901 <phillydooper@gmail.com>
2023-02-18 15:37:53 -0500

    Add the model used to set the lora script flag
    Add a model that is used to read and update the lora script thread
    running flag. This will allow the Previous commits relating to start and
    termination of the lora script to work.

--- trackside_monitor_project/trackside_monitor_app/models.py
+++ trackside_monitor_project/trackside_monitor_app/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
+class LoraThread(models.Model):
+    thread_name = models.CharField(max_length=10)
+    thread_flag = models.BooleanField()
