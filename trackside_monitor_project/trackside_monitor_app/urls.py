88d76704646eda8ecb8878f5ca3e54b7848fb528
boa901 <phillydooper@gmail.com>
2023-02-18 15:21:13 -0500

    Add ability to start/stop lora script
    Create two new URLs for two new views. One view starts the lora script,
    and the other stops the lora script. The lora script checks a database
    flag and exits the loop if the flag is True. The view that stops the
    script sets this flag to true, finds the thread, and joins it to
    gracefully kill the process.

--- trackside_monitor_project/trackside_monitor_app/urls.py
+++ trackside_monitor_project/trackside_monitor_app/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('start-lora/', views.start_lora),
+    path('stop-lora/', views.stop_lora),
     path('', views.index),
 ]
--- trackside_monitor_project/trackside_monitor_app/views.py
+++ trackside_monitor_project/trackside_monitor_app/views.py
@@ -4,18 +4,59 @@ from django.http import HttpResponse
 from django.template import loader
 
 from .background_processes.gauge_test import emulate_lora
+from trackside_monitor_app.models import LoraThread
+
+
+def validate_db(flag):
+    objects = LoraThread.objects.all()
+    if objects.count() == 1:
+        return
+    objects.delete()
+    LoraThread(thread_name='lora', thread_flag=flag).save()
+
 
 def index(request):
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render())
+
+
+def start_lora(request):
     lora_running = False
 
+    # Check if the lora script is running
     for thread in threading.enumerate():
         if thread.getName() == 'lora':
             lora_running = True
             break
 
-    if not lora_running:
-        lora_listener = threading.Thread(target=emulate_lora, name='lora', daemon=True)
-        lora_listener.start()
+    # Ensure that the thread_flag is set properly
+    validate_db(not lora_running)
 
-    template = loader.get_template('index.html')
-    return HttpResponse(template.render())
+    if not lora_running:
+        lora_thread = LoraThread.objects.get(thread_name='lora')
+        lora_thread.thread_flag = False
+        lora_thread.save()
+
+        # Start the LoRa reading process in a separate thread
+        lora_thread_process = threading.Thread(
+            target=emulate_lora, name='lora', daemon=True)
+        lora_thread_process.start()
+
+    return HttpResponse(status=200)
+
+
+def stop_lora(request):
+    validate_db(True)
+
+    lora_thread = LoraThread.objects.get(thread_name='lora')
+    lora_thread.thread_flag = True
+    lora_thread.save()
+
+    for thread in threading.enumerate():
+        if thread.getName() == 'lora':
+            while (True):
+                if not thread.isAlive():
+                    thread.join()
+                    break
+
+    return HttpResponse(status=200)
