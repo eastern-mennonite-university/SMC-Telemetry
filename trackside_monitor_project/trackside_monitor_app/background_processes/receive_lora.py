d7b640025337db28710ab58c28b872e1a9b3094d
boa901 <phillydooper@gmail.com>
2023-03-07 18:02:48 +0000

    Read LoRa transmissions to update gauges
    Add a background process to read LoRa transmissions and send the data to the data stream. This is based largely off of the txrx script and the emulate_lora script.

---
+++ trackside_monitor_project/trackside_monitor_app/background_processes/receive_lora.py
@@ -0,0 +1,41 @@
+import time
+
+from trackside_monitor_app.models import LoraThread
+
+from django_eventstream import send_event
+
+# Import Blinka Libraries
+import busio
+from digitalio import DigitalInOut, Direction, Pull
+import board
+# Import RFM9x
+import adafruit_rfm9x
+
+# Configure LoRa Radio
+CS = DigitalInOut(board.CE1)
+RESET = DigitalInOut(board.D25)
+spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
+rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, 915.0)
+rfm9x.tx_power = 23
+prev_packet = None
+
+def flag_check():
+    lora_thread = LoraThread.objects.get(thread_name='lora')
+    return lora_thread.thread_flag
+
+def receive_transmissions():
+    while True:
+        packet = rfm9x.receive()
+        if packet:
+            packet_data = str(packet, 'utf-8').split(',')
+            send_event('driving-data', 'message', {
+                'rpm': int(packet_data[0]),
+                'speed': 0,
+                'voltage': int(packet_data[5]),
+                'o2s': int(packet_data[4]),
+                'econ': int(packet_data[1]),
+                'temp-air': int(packet_data[3]),
+                'temp-engine': int(packet_data[2]),
+            })
+        if flag_check():
+            break
