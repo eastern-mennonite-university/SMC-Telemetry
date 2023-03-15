19fff1b5841582d34ea5482337e4e784c66517fb
boa901 <phillydooper@gmail.com>
2023-03-15 15:53:22 +0000

    Change DGPSread baudrate
    Change the baudrate for DGPSread from 9600 (default) to 115200, as this
    is the baudrate for the Arduino's serial output.

--- DGPSread.py
+++ DGPSread.py
@@ -5,7 +5,7 @@ serGPS = None
 def GPSdata():
     global serGPS
     if not serGPS:
-        serGPS = serial.Serial('/dev/ttyACM0', 9600)
+        serGPS = serial.Serial('/dev/ttyACM0', 115200)
     newbytes = serGPS.inWaiting()
     speed = str(serGPS.read(newbytes), 'UTF-8')
     if len(speed) > 0:
