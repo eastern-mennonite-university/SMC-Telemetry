61fcb3496bb71ff34d6fd27e64e3367a8fd9e8fe
boa901 <phillydooper@gmail.com>
2023-03-20 23:14:03 +0000

    Catch diconnect errors between pi and arduino
    Catch SerialException and OSErrors when connecting to the arduino and
    reading from the input buffer. This prevents the program from coming to
    a halt if the arduino fails or if the connection to the arduino fails.

--- DGPSread.py
+++ DGPSread.py
@@ -3,15 +3,21 @@ import serial
 serGPS = None
 
 def GPSdata():
-    global serGPS
-    if not serGPS:
-        serGPS = serial.Serial('/dev/ttyACM0', 115200)
-        serGPS.reset_input_buffer()
-    if serGPS.in_waiting > 0:
-        speed = serGPS.read(serGPS.in_waiting)
-        serGPS.reset_input_buffer()
+    try:
+        global serGPS
+        if not serGPS:
+            serGPS = serial.Serial('/dev/ttyACM0', 115200)
+            serGPS.reset_input_buffer()
         try:
-            return float(speed.decode())
-        except ValueError:
+            if serGPS.in_waiting > 0:
+                speed = serGPS.read(serGPS.in_waiting)
+                serGPS.reset_input_buffer()
+                try:
+                    return float(speed.decode())
+                except ValueError:
+                    pass
+        except OSError:
             pass
+    except serial.serialutil.SerialException:
+        pass
     return None
