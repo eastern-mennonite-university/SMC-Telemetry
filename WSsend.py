c718a54a350f7fe491a0d250af7126d68dcfa8b1
boa901 <phillydooper@gmail.com>
2023-04-13 20:19:37 +0100

    Remove ecu flag
    Remove the ecu flag boolean from WSsend. This flag is no longer
    necessary.

--- WSsend.py
+++ WSsend.py
@@ -1,3 +1,4 @@
+import time
 import asyncio
 import json
 import websockets
@@ -7,8 +8,6 @@ from DGPSread import *
 
 payload_data = dict()
 
-ecu_flag = False
-
 async def send_data():
     url = 'ws://localhost:8000/send_data/'
     async with websockets.connect(url) as websocket:
@@ -16,14 +15,13 @@ async def send_data():
             ecu_data = ECUdata()
             if ecu_data:
                 payload_data.update(ecu_data)
-                ecu_flag = True
 
             gps_data = GPSdata()
             if gps_data:
                 payload_data['speed'] = str(gps_data)
 
-            if ecu_flag:
-                await websocket.send(json.dumps(payload_data))
+            await websocket.send(json.dumps(payload_data))
+            time.sleep(0.1)
 
 if __name__ == '__main__':
     asyncio.run(send_data())
