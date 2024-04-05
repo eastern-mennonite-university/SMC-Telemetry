80cd920c02aef429de9bc38753b0643b4e6a7b83
64BitUniverse <64bituniverse@gmail.com>
2024-04-05 17:39:33 -0400

    Using WWsend.py as the brain

--- WSsend.py
+++ WSsend.py
@@ -2,26 +2,42 @@ import time
 import asyncio
 import json
 import websockets
+import tm1637
 
 from DECUread import *
 from DGPSread import *
 
+#initialize TM1637 display
+tm_RPM = tm1637.TM1637(clk = 20, dio = 19) #choose display pins
+tm_speed = tm1637.TM1637(clk = 18, dio = 17) #choose display pins
+
+rpm = None
+speed = None
+
 payload_data = dict()
 
 async def send_data():
-    url = 'wss://84436788-f699-4fda-98e3-4f0bed88db2b-00-u8599ttrqb23.picard.replit.dev/'
-    async with websockets.connect(url) as websocket:
-        while True:
-            ecu_data = ECUdata()
-            if ecu_data:
-                payload_data.update(ecu_data)
+    try:
+        url = 'wss://84436788-f699-4fda-98e3-4f0bed88db2b-00-u8599ttrqb23.picard.replit.dev/'
+        async with websockets.connect(url) as websocket:
+            while True:
+                ecu_data = ECUdata()
+                if ecu_data:
+                    payload_data.update(ecu_data)
 
-            gps_data = GPSdata()
-            if gps_data:
-                payload_data['speed'] = str(gps_data)
+                gps_data = GPSdata()
+                if gps_data:
+                    payload_data['speed'] = str(gps_data)
 
-            await websocket.send(json.dumps(payload_data))
-            time.sleep(0.1)
+                await websocket.send(json.dumps(payload_data))
+                time.sleep(0.1)
+    except Exception as e:
+        print(e)
+
+def Dashboard():
+    speed = GPSdata()*1.151
+    tm_speed.number(speed)
 
 if __name__ == '__main__':
     asyncio.run(send_data())
+    Dashboard()
